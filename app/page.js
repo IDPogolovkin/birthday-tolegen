2c1fcaf800861ede4b5df03b17e3c75d2ea7a9eb
IDPogolovkin <daniilpogolovkin@gmail.com>
2025-03-13 11:09:05 +0500

    add client whishes 4

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ const CountDown = dynamic(() => import('./components/CountDown'), {
 })
 export default function Home() {
 
-  const [play, setPlay] = useState(false)
+  const [play, setPlay] = useState(true)
 
   const playMusic = () => {
     setPlay(!play);
@@ -36,8 +36,8 @@ export default function Home() {
   <div className='font-[wg] text-center'>
     <main className='flex flex-col items-center justify-between relative '>
       <img src='/super-hero.jpeg' alt='Hero' className='w-full'/>
-      <img src='/oyu2.png' alt='stars' className=' absolute h-60 mt-2 ml-36 p-0 rotate-[-90deg] clip-diagonal-reverse'/>
-      {/* <img src='/oyu2.png' alt='stars' className=' absolute bottom-2 h-60 mt-2 mr-36 p-0 rotate-[90deg] clip-diagonal-reverse'/> */}
+      <img src='/oyu2.png' alt='stars' className=' absolute h-52 mt-2 ml-40 p-0 rotate-[-90deg] clip-diagonal-reverse'/>
+      <img src='/oyu2.png' alt='stars' className=' absolute bottom-72 w-40 h-40 mr-52 p-0 rotate-[90deg] clip-diagonal-reverse'/>
       <div className=' absolute top-0 left-8 w-[80%] text-white'>
         <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }}  className='text-left relative'>
           <p className={`${wg.className} text-4xl mt-6 m-0 font-extrabold`}>Толеген</p>
@@ -46,8 +46,8 @@ export default function Home() {
       </div>
         <div className=' font-bold  w-[88%] max-w-[370px] justify-between text-black h-[100%] mt-2'>
           <audio src="./music.mp3" autoPlay loop id='audio'></audio>
-          <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }}   className=' absolute bottom-48 right-[13%] z-15 mt-2'>
-            <div className='flex justify-center items-center mb-16'>
+          <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }}   className=' absolute bottom-36 right-[13%] z-15 mt-2'>
+            <div className='flex justify-center items-center mb-14'>
               { play ? 
               <button onClick={playMusic} className={`rounded-full bg-white/70 hover:bg-white/90 ${play && 'animate-[spin_3s_linear_infinite]'}`}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-12 w-12" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -64,23 +64,19 @@ export default function Home() {
               }
             </div>
           </motion.div>
-          <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className='mb-4 text-center  p-3 rounded-lg text-2xl bg-[rgba(0,0,0,0.5)]'>
+          <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className='mb-4 text-center  p-3 rounded-lg text-2xl '>
               <h1 className='text-4xl text-[#c1af3b] w-auto mb-2'>Құрметті</h1>
-              <i> <p className=' text-lg text-[#fff]'>Ағайын-туыс, құда-жекжат, нағашы-жиен, бөлелер, дос-жаран, көршілер! 
-                                          Сіздерді, Асқар таудай әкеміз Төлеген Аманкенұлы
+              <i> <p className=' text-lg '>Ағайын-туыс, құда-жекжат, нағашы-жиен, бөлелер, дос-жаран, көршілер! 
+                                          Сіздерді, Асқар таудай әкеміз Төлеген Аманкенұлы 70 жас мерей тойына арналған салтанатты кешіміздің қадірлі қонағы болуға шақырамыз!
               </p></i>
             </motion.div>
-            <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className=' mt-2 text-center  p-3 rounded-lg text-2xl bg-[rgba(0,0,0,0.5)]'>
-              <i> <p className=' text-lg text-[#fff]'>70 жас мерей тойына арналған салтанатты кешіміздің қадірлі қонағы болуға шақырамыз!
-              </p></i>
-          </motion.div>
       </div>
     </main>
 
     <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }}   className=' flex flex-col w-[80%] mx-auto p-4 rounded-lg relative overflow-hidden right-0 justify-center items-center text-lg mt-4'>
       <p className=' text-3xl font-extrabold'>Aisara Hall</p>
       <p className=' text-2xl font-extrabold'>Банкет залы</p>
-      <img src="/aisara.png" alt="aisara" />
+      <img src="/aisara.png" alt="aisara"/>
       <img src="/favicon.png" alt="2gis" className=' h-[75px] w-[75px] mt-4 cursor-pointer' onClick={(e) => {
             window.location.href = 'https://go.2gis.com/ZQSrG'
           }}/>
@@ -92,14 +88,8 @@ export default function Home() {
         <i><p className={`text-2xl mt-6 m-0 absolute top-0 right-5`}>10.05.2025 | 13:00 | сенбі</p></i>
     </motion.div>
 
-    <div className='relative flex justify-center mb-[330px] bg-mramor'>
-        <motion.img initial={{ rotate: 0, x: -100, opacity: 0 }} whileInView={{ opacity: 1, rotate: 20, x: 0 }} transition={{ duration: 1 }} src='/3.jpeg' alt='Section' className='object-cover w-[40%] mx-auto mt-10   rounded-lg absolute rotate-[20deg] top-10 right-5 h-[200px]'/>
-        <motion.img initial={{ rotate: 0, x: 100, opacity: 0 }} whileInView={{ opacity: 1, rotate: -18, x: 0  }} transition={{ duration: 1 }} src='/3.jpeg' alt='Section' className='object-cover w-[40%] mx-auto mt-10 rounded-lg absolute -rotate-[18deg] top-10 left-7 h-[200px]'/>
-        <motion.img src='2.JPG' alt='Section' className=' object-cover w-[40%] mx-auto mt-10  rounded-lg absolute h-[250px]'/>
-    </div>
-
     <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }}   className=' flex flex-col w-[80%] mx-auto mb-[20px] ] p-4 rounded-lg relative overflow-hidden right-0 justify-center items-center text-lg '>
-      <img src="/wh.png" alt="2gis" className=' h-[75px] w-[75px] mt-7 cursor-pointer' onClick={(e) => {
+      <img src="/wh.png" alt="2gis" className=' h-[75px] w-[75px] mt-12 cursor-pointer' onClick={(e) => {
             window.location.href = 'https://wa.me/77015359896'
           }}/>
       <p className=' text-2xl font-extrabold'>WhatsApp арқалы байланысу</p>
@@ -108,10 +98,16 @@ export default function Home() {
     <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className='w-[80%] font-bold mx-auto text-center relative mb-[35px] border-[#96825f] border-[1px] '>
     </motion.div>
 
-    <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className='pb-12'>
+    <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: -100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }} className='pb-1'>
       <h1 className='text-3xl font-extrabold'>Той иелері:</h1>
       <i><p className='text-3xl'>жұбайы, балалары, немерелері!</p></i>
     </motion.div>
+
+    <div className='relative flex justify-center mb-[330px] bg-mramor'>
+        <motion.img initial={{ rotate: 0, x: -100, opacity: 0 }} whileInView={{ opacity: 1, rotate: 20, x: 0 }} transition={{ duration: 1 }} src='/3.jpeg' alt='Section' className='object-cover w-[40%] mx-auto mt-10   rounded-lg absolute rotate-[20deg] top-10 right-5 h-[200px]'/>
+        <motion.img initial={{ rotate: 0, x: 100, opacity: 0 }} whileInView={{ opacity: 1, rotate: -18, x: 0  }} transition={{ duration: 1 }} src='/3.jpeg' alt='Section' className='object-cover w-[40%] mx-auto mt-10 rounded-lg absolute -rotate-[18deg] top-10 left-7 h-[200px]'/>
+        <motion.img src='2.JPG' alt='Section' className=' object-cover w-[40%] mx-auto mt-10  rounded-lg absolute h-[250px]'/>
+    </div>
 
     <motion.div whileInView={{ opacity: 1, y: 0 }} initial={{ opacity: 0, y: 100 }} exit={{ opacity: 0, y: -100 }} transition={{ duration: 1 }}  className='w-[80%] mx-auto mb-16'>
       <img src="/1.png" alt="Photo" className=' ml-8 rounded-3xl mx-auto'/>
